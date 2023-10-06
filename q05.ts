71ded5f181dc3b00d7b04df7c0c049a78509427a
Muhammad Waqas <139634396+waqas3520@users.noreply.github.com>
2023-10-06 12:26:18 +0500

    Add files via upload

---
+++ q05.ts
@@ -0,0 +1,14 @@
+//Name  Muhammad Waqas
+//Roll No   PIAIC215686
+//Batch Batch 47
+
+// Famous Quote 2: Repeat Exercise 4,
+//  but this time store the famous person’s name in a variable called famous_person. 
+//  Then compose your message and store it in a new variable called message. Print your message.
+
+
+
+let quote1: string = "A person who never made a mistake never tried anything new.";
+let famous_person: string = "Albert Einstein";
+let message: string = `${famous_person} once said, "${quote1}"`;
+console.log(message);
